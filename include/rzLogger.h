87f0775e25122ebd147688e25fcbd281ea88e62b
David Ruiz <daavidruiz01@outlook.com>
2025-10-26 15:30:55 +0100

    C++ Support

--- include/rzLogger.h
+++ include/rzLogger.h
@@ -1,13 +1,9 @@
 #ifndef RZ_LOGGER_H
 #define RZ_LOGGER_H
 
-#define rzLog(level, format, ...) \
-    rzLog_impl(level, __FILE__, __LINE__, __FUNCTION__, format, ##__VA_ARGS__)
-
-#define rzERROR(format, ...) rzTraza(RZ_TRACE_ERROR, format, ##__VA_ARGS__)
-#define rzWARN(format, ...)  rzTraza(RZ_TRACE_WARN,  format, ##__VA_ARGS__)  
-#define rzINFO(format, ...)  rzTraza(RZ_TRACE_INFO,  format, ##__VA_ARGS__)
-#define rzDEBUG(format, ...) rzTraza(RZ_TRACE_DEBUG, format, ##__VA_ARGS__)
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef enum {
     RZ_LOG_ERROR        = 1,
@@ -17,11 +13,27 @@ typedef enum {
     RZ_LOG_DEBUG_EXTRA  = 5
 } rz_logger_level_t;
 
+// Función principal de logging
 void rzLog_impl(rz_logger_level_t level, const char* file, int line, const char* func, const char* format, ...);
 
+// Macro para uso conveniente
+#define rzLog(level, format, ...) \
+    rzLog_impl(level, __FILE__, __LINE__, __FUNCTION__, format, ##__VA_ARGS__)
+
+// Macros de niveles de logging
+#define rzERROR(format, ...) rzLog_impl(RZ_LOG_ERROR, __FILE__, __LINE__, __FUNCTION__, format, ##__VA_ARGS__)
+#define rzWARN(format, ...)  rzLog_impl(RZ_LOG_WARN,  __FILE__, __LINE__, __FUNCTION__, format, ##__VA_ARGS__)  
+#define rzINFO(format, ...)  rzLog_impl(RZ_LOG_INFO,  __FILE__, __LINE__, __FUNCTION__, format, ##__VA_ARGS__)
+#define rzDEBUG(format, ...) rzLog_impl(RZ_LOG_DEBUG, __FILE__, __LINE__, __FUNCTION__, format, ##__VA_ARGS__)
+
+// Inicialización y configuración
 void rzLog_init();
 void rzLog_stop();
-void rzLog_set_level(rz_logger_level_t min_level); // NULL to ENV variable
-void rzLog_set_output(const char* filename); //NULL to console
+void rzLog_set_level(rz_logger_level_t min_level); // NULL para leer de variable ENV
+void rzLog_set_output(const char* filename); // NULL para consola
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif // RZ_LOGGER_H
